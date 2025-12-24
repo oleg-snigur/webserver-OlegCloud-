ef2828f2171f76d4e46c9b586583ce78f31c5c07
oleg-snigur <sniguroleg77@gmail.com>
2025-12-24 11:48:31 +0200

    add ssl certificate

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 import database, models
 from auth import router as auth_router
 from files import router as files_router
@@ -10,9 +11,7 @@ app = FastAPI()
 async def startup_event():
     await init_bucket()
 
-@app.get("/")
-def root():
-    return {"message": "Backend працює!"}
-
-app.include_router(auth_router, prefix="/api")   # Тепер ловить /api/login
+app.include_router(auth_router, prefix="/api")
 app.include_router(files_router, prefix="/api")
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
